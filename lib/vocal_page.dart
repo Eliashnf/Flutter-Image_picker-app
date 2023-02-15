1df77f48c0c3958edec6c1d5623f8aefc519490a
Eliashnf <eliashnf@gmail.com>
2023-02-15 06:23:41 -0800

    Adding the dependencie speech_to_text and the feature

--- lib/vocal_page.dart
+++ lib/vocal_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class VocalPage extends StatefulWidget {
   @override
@@ -6,6 +7,39 @@ class VocalPage extends StatefulWidget {
 }
 
 class _VocalPageState extends State<VocalPage> {
+  final stt.SpeechToText _speech = stt.SpeechToText();
+  bool _isListening = false;
+  String _text = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeechRecognizer();
+  }
+
+  void _initSpeechRecognizer() async {
+    bool available = await _speech.initialize(
+      onStatus: (status) => print('onStatus: $status'),
+      onError: (error) => print('onError: $error'),
+    );
+
+    if (available) {
+      setState(() => _isListening = true);
+      _speech.listen(
+        onResult: (result) => setState(() {
+          _text = result.recognizedWords;
+        }),
+      );
+    } else {
+      print('The user has denied the use of speech recognition.');
+    }
+  }
+
+  void _stopSpeechRecognizer() {
+    _speech.stop();
+    setState(() => _isListening = false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +47,24 @@ class _VocalPageState extends State<VocalPage> {
         title: Text('Vocal Page'),
       ),
       body: Center(
-        child: Text('This is the vocal page.'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Start listening'),
+              onPressed: () {
+                if (!_isListening) {
+                  _initSpeechRecognizer();
+                }
+              },
+            ),
+            ElevatedButton(
+              child: Text('Stop listening'),
+              onPressed: _isListening ? _stopSpeechRecognizer : null,
+            ),
+            Text(_text),
+          ],
+        ),
       ),
     );
   }
